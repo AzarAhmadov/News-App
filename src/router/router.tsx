1832ee965b4d2a6394fc1f18818520fdb6a5b3e9
AzarAhmadov <ehmedovh111@gmail.com>
2024-08-23 22:45:56 +0400

    Create router.tsx

---
+++ src/router/router.tsx
@@ -0,0 +1,23 @@
+import { createBrowserRouter } from "react-router-dom";
+import Home from "../layout/Home";
+import NewsBlock from "../components/share/NewsBlock/NewsBlock";
+import NewsDetail from "../components/share/NewsDetail/NewsDetail";
+
+const Routers = createBrowserRouter([
+    {
+        path: "/",
+        element: <Home />,
+        children: [
+            {
+                index: true,
+                element: <NewsBlock />,
+            },
+            {
+                path: "/detail",
+                element: <NewsDetail />,
+            },
+        ],
+    },
+]);
+
+export default Routers
