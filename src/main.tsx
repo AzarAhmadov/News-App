abeff12d2bde79bc821692afa61f0dd686e354e6
AzarAhmadov <ehmedovh111@gmail.com>
2024-08-23 22:45:53 +0400

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,9 @@
 
 import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
 import './assets/css/index.css'
+import { RouterProvider } from 'react-router-dom'
+import Routers from './router/router.tsx'
 
 createRoot(document.getElementById('root')!).render(
-  <>
-    <App />
-  </>,
+  <RouterProvider router={Routers} />
 )
