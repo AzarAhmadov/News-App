336990966f7daf83063146181797c827dbe5c68e
AzarAhmadov <ehmedovh111@gmail.com>
2024-08-25 00:53:26 +0400

    Update NewsBlock.tsx

--- src/components/share/NewsBlock/NewsBlock.tsx
+++ src/components/share/NewsBlock/NewsBlock.tsx
@@ -1,7 +1,15 @@
 import React from "react";
+import NewsBlockItem from "./NewsBlockItem/NewsBlockItem";
 
 const NewsBlock: React.FC = () => {
-  return <div>NewsBlock</div>;
+  return (
+    <div className="grid grid-cols-2 gap-4 py-5">
+      <NewsBlockItem />
+      <NewsBlockItem />
+      <NewsBlockItem />
+      <NewsBlockItem />
+    </div>
+  );
 };
 
 export default NewsBlock;
