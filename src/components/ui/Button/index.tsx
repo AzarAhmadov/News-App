ca491fd393b79f304c4af4f64e681a03ae3eff5c
AzarAhmadov <ehmedovh111@gmail.com>
2024-08-25 00:09:48 +0400

    Update index.tsx

--- src/components/ui/Button/index.tsx
+++ src/components/ui/Button/index.tsx
@@ -1,22 +1,21 @@
-import React, { memo } from 'react';
-import classNames from 'classnames';
-import './button.css'
+import React, { memo } from "react";
+import classNames from "classnames";
+import "./button.css";
 
 const Button: React.FC<any> = ({ children, type, rounded, size }) => {
-
-    console.log(`btn-${type}`)
-
-    return (
-        <button className={classNames(
-            {
-                'btn': true,
-                'btn-rounded': rounded,
-            },
-            [`btn-${type}`, `btn-${size}`]
-        )}>
-            {children}
-        </button>
-    );
-}
+  return (
+    <button
+      className={classNames(
+        {
+          btn: true,
+          "btn-rounded": rounded,
+        },
+        [`btn-${type}`, `btn-${size}`],
+      )}
+    >
+      {children}
+    </button>
+  );
+};
 
 export default memo(Button);
