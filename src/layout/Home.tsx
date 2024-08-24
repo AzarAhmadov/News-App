a5f686bb3bf5347e0f448c400427d4a1e0cafd9b
AzarAhmadov <ehmedovh111@gmail.com>
2024-08-25 00:09:41 +0400

    Update Home.tsx

--- src/layout/Home.tsx
+++ src/layout/Home.tsx
@@ -2,12 +2,14 @@ import React from 'react'
 import Aside from '../components/common/Aside/Aside'
 import RightBlock from '../components/common/RightBlock/RightBlock'
 import { Outlet } from 'react-router-dom'
+import Search from '../components/common/Seach/Search'
 
 const Home: React.FC = () => {
     return (
-        <div className="grid grid-cols-[260px_auto_285px] bg-bodyBack min-h-screen">
+        <div className="grid gap-5 grid-cols-[16.25rem_auto_17.813rem] bg-bodyBack min-h-screen">
             <Aside />
-            <main className='mx-auto'>
+            <main className='py-8'>
+                <Search />
                 <Outlet />
             </main>
             <RightBlock />
