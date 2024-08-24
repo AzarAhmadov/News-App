928ccf27f046090c749c50f5b9ea6f840a63c17a
AzarAhmadov <ehmedovh111@gmail.com>
2024-08-24 21:01:56 +0400

    add Menu

---
+++ src/constants/constants.ts
@@ -0,0 +1,26 @@
+export const Menu = [
+    {
+        name: "Top Stories",
+        to: "/"
+    },
+    {
+        name: "Around the World",
+        to: "/around-the-world"
+    },
+    {
+        name: "Business",
+        to: "/business"
+    },
+    {
+        name: "Health",
+        to: "/health"
+    },
+    {
+        name: "Sports",
+        to: "/sports"
+    },
+    {
+        name: "Discussion",
+        to: "/discussion"
+    },
+]
