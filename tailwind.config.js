b6f471d7fcb2c5a9e09712fba041074636fe9abd
AzarAhmadov <ehmedovh111@gmail.com>
2024-08-25 00:53:10 +0400

    add new colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       colors: {
-        primaryDarker: '#0768B5',
-        primaryDark: '#0768B5',
-        primary: '#2F9FF8',
-        amberBlack: '#072D4B',
-        bodyBack:'#F4F9F8',
-        activeLink:'#E0F0F8',
-        textWhite:'#fff',
-        inputBg:"#ECF5F8"
+        primaryDarker: "#0768B5",
+        primaryDark: "#0768B5",
+        primary: "#2F9FF8",
+        amberBlack: "#072D4B",
+        bodyBack: "#F4F9F8",
+        activeLink: "#E0F0F8",
+        textWhite: "#fff",
+        inputBg: "#ECF5F8",
+        paragraphColor: "#6A8193",
+        infoText: "#9CABB7",
       },
     },
   },
   plugins: [],
-}
+};
