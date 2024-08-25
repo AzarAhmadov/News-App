341e03e109e8085ef1cd66cab51ed3226ec8e965
AzarAhmadov <ehmedovh111@gmail.com>
2024-08-25 18:18:37 +0400

    Update Home.tsx

--- src/layout/Home.tsx
+++ src/layout/Home.tsx
@@ -2,15 +2,21 @@ import React from "react";
 import Aside from "../components/common/Aside/Aside";
 import { Outlet } from "react-router-dom";
 import Header from "../components/common/Header/Header";
+import Footer from "../components/common/Footer/Footer";
 
 const Home: React.FC = () => {
   return (
     <div className="grid h-full grid-cols-[260px_calc(100%_-_325px)] gap-[35px] pt-3">
       <Aside />
-      <main className="container mx-auto">
-        <Header />
-        <Outlet />
-      </main>
+      <div className="container flex flex-col justify-between h-screen mx-auto">
+        <div>
+          <Header />
+          <main>
+            <Outlet />
+          </main>
+        </div>
+        <Footer />
+      </div>
     </div>
   );
 };
