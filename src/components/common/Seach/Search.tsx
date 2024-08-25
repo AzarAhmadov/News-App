25f4ccd21738dcff90fe9af8302269ce8b8f74dc
AzarAhmadov <ehmedovh111@gmail.com>
2024-08-25 15:13:45 +0400

    Update Search.tsx

--- src/components/common/Seach/Search.tsx
+++ src/components/common/Seach/Search.tsx
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 const Search: React.FC = () => {
   return (
     <div className="flex items-center gap-x-5">
-      <form className="flex items-center justify-between w-full px-3 py-3 border border-gray-300 rounded-md gap-x-3 bg-inputBg">
+      <form className="flex w-full items-center justify-between gap-x-3 rounded-md border border-gray-300 bg-inputBg px-3 py-3">
         <input
           className="w-full bg-transparent outline-none"
           type="text"
@@ -15,7 +15,7 @@ const Search: React.FC = () => {
       </form>
 
       <Link
-        className="flex items-center px-6 py-3 transition-all border border-gray-300 rounded-md gap-x-4 whitespace-nowrap hover:bg-gray-200"
+        className="flex items-center gap-x-4 whitespace-nowrap rounded-md border border-gray-300 px-6 py-3 transition-all hover:bg-gray-200"
         to={"/"}
       >
         <p className="flex gap-x-2">
