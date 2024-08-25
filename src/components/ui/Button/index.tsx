60059c7e8746657ea77053f83c893c94a1202e61
AzarAhmadov <ehmedovh111@gmail.com>
2024-08-25 15:13:25 +0400

    Update index.tsx

--- src/components/ui/Button/index.tsx
+++ src/components/ui/Button/index.tsx
@@ -3,15 +3,23 @@ import classNames from "classnames";
 import "./button.css";
 import { TypeButton } from "../../../types/types";
 
-const Button: React.FC<TypeButton> = ({ children, type, rounded, size }) => {
+const Button: React.FC<TypeButton> = ({
+  children,
+  type,
+  rounded,
+  size,
+  width,
+  outline,
+}) => {
   return (
     <button
       className={classNames(
         {
           btn: true,
           "btn-rounded": rounded,
+          "btn-outline": outline,
         },
-        [`btn-${type}`, `btn-${size}`],
+        [`btn-${type}`, `btn-${size}`, `btn-${width}`],
       )}
     >
       {children}
