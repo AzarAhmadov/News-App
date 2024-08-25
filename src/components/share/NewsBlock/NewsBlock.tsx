d730ed0298d80cf664bc740ad873898104f85dbb
AzarAhmadov <ehmedovh111@gmail.com>
2024-08-26 00:08:15 +0400

    Update NewsBlock.tsx

--- src/components/share/NewsBlock/NewsBlock.tsx
+++ src/components/share/NewsBlock/NewsBlock.tsx
@@ -1,22 +1,24 @@
 import React from "react";
 import NewsBlockItem from "./NewsBlockItem/NewsBlockItem";
 import { typeNewsBlock } from "../../../types/types";
+import { useFetchAllNews } from "../../../hooks/useFetch";
 
 const NewsBlock: React.FC<typeNewsBlock> = ({
   title = "Around the world",
   icon,
 }) => {
+  const [data] = useFetchAllNews();
+
   return (
     <>
-      <div className="flex items-center mt-6 gap-x-2">
+      <div className="mt-6 flex items-center gap-x-2">
         {icon}
         <h2 className="text-lg font-medium"> {title} </h2>
       </div>
       <div className="grid grid-cols-3 gap-3 py-5">
-        <NewsBlockItem />
-        <NewsBlockItem />
-        <NewsBlockItem />
-        <NewsBlockItem />
+        {data?.data?.map((item: any, index: number) => (
+          <NewsBlockItem item={item} key={index} />
+        ))}
       </div>
     </>
   );
