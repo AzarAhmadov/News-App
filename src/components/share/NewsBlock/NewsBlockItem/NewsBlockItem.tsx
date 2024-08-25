9c56b0253a9523206be3baec725a3d13a151b1f5
AzarAhmadov <ehmedovh111@gmail.com>
2024-08-26 00:08:11 +0400

    Update NewsBlockItem.tsx

--- src/components/share/NewsBlock/NewsBlockItem/NewsBlockItem.tsx
+++ src/components/share/NewsBlock/NewsBlockItem/NewsBlockItem.tsx
@@ -1,38 +1,40 @@
 import React from "react";
 import { AiOutlineShareAlt } from "react-icons/ai";
 import { Link } from "react-router-dom";
+import moment from "moment";
 
-const NewsBlockItem: React.FC = () => {
+const NewsBlockItem: React.FC<any> = ({ item }) => {
   return (
     <article className="p-5 transition-all bg-white border border-gray-200 rounded-md shadow-sm hover:scale-105">
       <div className="relative flex gap-x-4">
         <div>
           <h3 className="text-lg font-medium leading-7 line-clamp-2 text-amberBlack">
-            Battlegrounds Mobile India iOS release date
+            {item.title}
           </h3>
-          <p className="mt-2 text-sm font-normal leading-6 text-paragraphColor line-clamp-3">
-            The reason behind their disappointment is that iPhone users have
-            been..
+          <p className="mt-2 text-sm font-normal leading-6 line-clamp-3 text-paragraphColor">
+            {item.description}
           </p>
         </div>
         <div>
           <figure className="aspect-[1/1] h-[132px] shrink-0 overflow-hidden rounded-[4px]">
             <img
               className="object-cover size-full"
-              src="https://images.pexels.com/photos/26690031/pexels-photo-26690031/free-photo-of-back-view-of-atlantic-puffin-on-rock.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load"
-              alt=""
+              src={item.photo}
+              alt={item.author.fullname}
             />
           </figure>
         </div>
-        <Link to={"/detail/name"}  className="absolute inset-0"/>
+        <Link to={`/detail/${item.slug}`} className="absolute inset-0" />
       </div>
       <ul className="flex justify-between mt-5">
-        <li className="flex items-center text-[0.813rem] text-infoText gap-x-2">
-          <span>Sport Biz</span>
+        <li className="flex items-center gap-x-2 text-[0.813rem] text-infoText">
+          <span>{item.author.agency}</span>
           <span>●</span>
-          <span>15 mins ago</span>
+          <span>
+            {moment(item.published_date, "YYYYMMDD").fromNow()}
+          </span>
         </li>
-        <li className="flex text-[0.813rem] gap-x-3">
+        <li className="flex gap-x-3 text-[0.813rem]">
           <button className="flex items-center gap-x-2">
             <AiOutlineShareAlt />
             Share
