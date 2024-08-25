3c1a7f4744f9f45014776de2b8dac5b55880bc61
AzarAhmadov <ehmedovh111@gmail.com>
2024-08-26 01:10:14 +0400

    Update CenterBlock.tsx

--- src/components/share/CenterBlock/CenterBlock.tsx
+++ src/components/share/CenterBlock/CenterBlock.tsx
@@ -3,13 +3,15 @@ import Slide from "../../ui/Swiper";
 import { LuPencilLine } from "react-icons/lu";
 import { SwiperSlide } from "swiper/react";
 import NewsBlock from "../NewsBlock/NewsBlock";
-import { SliderItems } from "../../../constants/constants";
 import Button from "../../ui/Button";
 import { MdArrowForwardIos } from "react-icons/md";
 import { BiWorld } from "react-icons/bi";
 import { FaRegNewspaper } from "react-icons/fa6";
+import { useFetchAutors } from "../../../hooks/useFetch";
 
 const CenterBlock: React.FC = () => {
+  const [data] = useFetchAutors();
+
   return (
     <>
       <NewsBlock
@@ -17,20 +19,20 @@ const CenterBlock: React.FC = () => {
         icon={<BiWorld className="text-xl" />}
       />
       <Slide title="Creators you should follow" icon={<LuPencilLine />}>
-        {SliderItems.map((item, index) => (
+        {data?.data?.map((item: any, index: number) => (
           <SwiperSlide key={index}>
             <div className="flex flex-col items-center p-4 bg-white rounded-md gap-y-3">
               <figure className="size-[85px] overflow-hidden rounded-full border border-black/10 p-1">
                 <img
                   className="object-cover rounded-full size-full"
-                  src={item.img}
-                  alt={item.name}
+                  src={item.photo}
+                  alt={item.fullname}
                 />
               </figure>
               <div className="text-center">
-                <h3 className="text-md line-clamp-1">{item.name}</h3>
+                <h3 className="text-md line-clamp-1">{item.fullname}</h3>
                 <span className="text-sm line-clamp-1 text-paragraphColor">
-                  {item.info}
+                  {item.agency}
                 </span>
               </div>
               <Button
