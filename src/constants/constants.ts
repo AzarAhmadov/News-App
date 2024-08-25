b79b1a59f3b36964bace49a8bc7bde6ab725bcbe
AzarAhmadov <ehmedovh111@gmail.com>
2024-08-25 15:13:08 +0400

    add SliderItems

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -24,3 +24,41 @@ export const Menu = [
     to: "/discussion",
   },
 ];
+
+export const SliderItems = [
+  {
+    name: "Shan Teylor",
+    info: "Tech Ming",
+    img: "https://s3-alpha-sig.figma.com/img/ac1e/f74c/931ce67584033f45f9412bcba54ac5f3?Expires=1725235200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=qPdymf7qCzCHPsdvzVXGi-tSFjkTYCI9rJFn3to-Zqs3T~4cEDSGl41YVwQJxh2fYApbvHktrb-aqu4EcZC2T-~EuFVjvAxgnq8ZvC6HX~gLgCK1hv4gpnu5YqQ0Kee33okpfVOlkMTwMdsXkLWlc-ZQdi9VwafimxLRtyu8jCBKlup5NK1hXSVM6lrhxOlmJBHciH2CfwhlUO9JRevu5uPSKsy94V6NLNWemsuvIM3zVj9VHqdbUkxKxsPmDigEShWqI3zC58vIu9A1Lpt2Tuwv~yJRlLwGm~M4YnIDm5eDOeCuc3CwIjjir17sxKtNTSR6ooKbO7xa4dDrbFHlLw__",
+  },
+  {
+    name: "Shan Teylor",
+    info: "Tech Ming",
+    img: "https://images.pexels.com/photos/2379004/pexels-photo-2379004.jpeg?auto=compress&cs=tinysrgb&w=800",
+  },
+  {
+    name: "Shan Teylor",
+    info: "Tech Ming",
+    img: "https://images.pexels.com/photos/874158/pexels-photo-874158.jpeg?auto=compress&cs=tinysrgb&w=800",
+  },
+  {
+    name: "Shan Teylor",
+    info: "Tech Ming",
+    img: "https://images.pexels.com/photos/1121796/pexels-photo-1121796.jpeg?auto=compress&cs=tinysrgb&w=800",
+  },
+  {
+    name: "Shan Teylor",
+    info: "Azar Ahmadov",
+    img: "https://images.pexels.com/photos/775358/pexels-photo-775358.jpeg?auto=compress&cs=tinysrgb&w=800",
+  },
+  {
+    name: "Shan Teylor",
+    info: "Tech Ming",
+    img: "https://s3-alpha-sig.figma.com/img/ac1e/f74c/931ce67584033f45f9412bcba54ac5f3?Expires=1725235200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=qPdymf7qCzCHPsdvzVXGi-tSFjkTYCI9rJFn3to-Zqs3T~4cEDSGl41YVwQJxh2fYApbvHktrb-aqu4EcZC2T-~EuFVjvAxgnq8ZvC6HX~gLgCK1hv4gpnu5YqQ0Kee33okpfVOlkMTwMdsXkLWlc-ZQdi9VwafimxLRtyu8jCBKlup5NK1hXSVM6lrhxOlmJBHciH2CfwhlUO9JRevu5uPSKsy94V6NLNWemsuvIM3zVj9VHqdbUkxKxsPmDigEShWqI3zC58vIu9A1Lpt2Tuwv~yJRlLwGm~M4YnIDm5eDOeCuc3CwIjjir17sxKtNTSR6ooKbO7xa4dDrbFHlLw__",
+  },
+  {
+    name: "Shan Teylor",
+    info: "Tech Ming",
+    img: "https://s3-alpha-sig.figma.com/img/ac1e/f74c/931ce67584033f45f9412bcba54ac5f3?Expires=1725235200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=qPdymf7qCzCHPsdvzVXGi-tSFjkTYCI9rJFn3to-Zqs3T~4cEDSGl41YVwQJxh2fYApbvHktrb-aqu4EcZC2T-~EuFVjvAxgnq8ZvC6HX~gLgCK1hv4gpnu5YqQ0Kee33okpfVOlkMTwMdsXkLWlc-ZQdi9VwafimxLRtyu8jCBKlup5NK1hXSVM6lrhxOlmJBHciH2CfwhlUO9JRevu5uPSKsy94V6NLNWemsuvIM3zVj9VHqdbUkxKxsPmDigEShWqI3zC58vIu9A1Lpt2Tuwv~yJRlLwGm~M4YnIDm5eDOeCuc3CwIjjir17sxKtNTSR6ooKbO7xa4dDrbFHlLw__",
+  },
+];
