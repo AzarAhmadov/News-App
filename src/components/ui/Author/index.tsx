bbae2209d2793097d8a70c2d3093924187faf8c5
AzarAhmadov <ehmedovh111@gmail.com>
2024-08-27 00:02:02 +0400

    Update index.tsx

--- src/components/ui/Author/index.tsx
+++ src/components/ui/Author/index.tsx
@@ -1,34 +1,57 @@
 import React, { useEffect } from "react";
 import { IoNewspaperOutline } from "react-icons/io5";
-import { useFetchAuthorBySlug } from "../../../hooks/useFetch";
+import {
+  useFetchAuthorBySlug,
+  useFetchNewsAuthorBySlug,
+} from "../../../hooks/useFetch";
 import { useParams } from "react-router-dom";
+import AuthorNews from "../AuthorNews";
+import GoBack from "../GoBack";
+import ErrorMsg from "../ErrorMsg";
+import Spinner from "../Spinner";
 
 const Author: React.FC = () => {
   const { slug } = useParams();
-  const [data] = useFetchAuthorBySlug(slug);
+  const [autorSlugData, loading] = useFetchAuthorBySlug(slug);
+  const [authorAllNewsData] = useFetchNewsAuthorBySlug(slug, 8);
+
+  if (loading) {
+    return <Spinner />;
+  }
+
+  if (!autorSlugData) {
+    return <ErrorMsg text="Axtardığınız müəllif mövcud deyil..." />;
+  }
 
   return (
-    <div className="flex items-center justify-between px-5 py-3 my-4 border rounded-md bg-inputBg">
-      <div className="flex items-center gap-x-3">
-        <figure className="size-[4.5rem] overflow-hidden rounded-full border border-primaryDarker/30 p-1">
-          <img
-            className="object-cover rounded-full size-full"
-            src={data?.photo}
-            alt={data?.fullname}
-          />
-        </figure>
-        <div>
-          <h4 className="mb-1 font-medium">Deontae Leannon</h4>
-          <span className="font-normal text-paragraphColor">
-            {data?.agency}
-          </span>
-        </div>
+    <>
+      <div className="mt-5">
+        <GoBack />
       </div>
-      <span className="flex items-center px-4 py-3 text-sm text-white bg-black rounded-md gap-x-2">
-        <IoNewspaperOutline className="text-md" />
-        Xəbər sayı: 20
-      </span>
-    </div>
+      <div className="flex items-center justify-between px-5 py-3 my-4 bg-white border rounded-md shadow-sm">
+        <div className="flex items-center gap-x-3">
+          <figure className="size-[4.5rem] overflow-hidden rounded-full border border-primaryDarker/30 p-1">
+            <img
+              className="object-cover rounded-full size-full"
+              src={autorSlugData?.photo}
+              alt={autorSlugData?.fullname}
+            />
+          </figure>
+          <div>
+            <h4 className="mb-1 font-medium">{autorSlugData?.fullname}</h4>
+            <span className="font-normal text-paragraphColor">
+              {autorSlugData?.agency}
+            </span>
+          </div>
+        </div>
+        <span className="flex items-center px-4 py-3 text-sm text-white bg-black rounded-md gap-x-2">
+          <IoNewspaperOutline className="text-md" />
+          Xəbər sayı: {authorAllNewsData?.total}
+        </span>
+      </div>
+
+      <AuthorNews authorAllNewsData={authorAllNewsData} />
+    </>
   );
 };
 
