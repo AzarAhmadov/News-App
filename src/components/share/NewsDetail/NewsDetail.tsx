818b1aa8e023c56c3e6b7ed8074c981e5a3ed61e
AzarAhmadov <ehmedovh111@gmail.com>
2024-08-26 12:50:32 +0400

    Update NewsDetail.tsx

--- src/components/share/NewsDetail/NewsDetail.tsx
+++ src/components/share/NewsDetail/NewsDetail.tsx
@@ -1,61 +1,53 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import Comment from "../../ui/Comment";
 import { FaArrowLeft } from "react-icons/fa";
+import { useFetchNewsBySlug } from "../../../hooks/useFetch";
+import moment from "moment";
 
 const NewsDetail: React.FC = () => {
+  const { slug } = useParams();
+
+  const [data] = useFetchNewsBySlug(slug);
+
   return (
     <>
       <div className="py-7">
-        <Link className="flex p-2 px-4 mb-5 text-white bg-black rounded-md w-max" to={"/"}>
+        <Link
+          className="mb-5 flex w-max rounded-md bg-black p-2 px-4 text-white"
+          to={"/"}
+        >
           <FaArrowLeft />
         </Link>
 
-        <h3 className="text-2xl text-black">
-          Leak: Samsung to announce the Z Fold 3 and Galaxy Watch 4 in August
-        </h3>
+        <h3 className="text-2xl text-black">{data?.title}</h3>
         <Link
-          className="inline-flex px-4 py-1 mt-3 rounded-md bg-activeLink text-primaryDark"
-          to={""}
+          className="mt-3 inline-flex rounded-md bg-activeLink px-4 py-1 text-primaryDark"
+          to={`/search?category=${data?.category?.slug}`}
         >
-          Mobile
+          {data?.category?.name}
         </Link>
       </div>
 
       <figure className="aspect-[16/8] overflow-hidden rounded-md">
         <img
-          className="object-cover object-center size-full"
-          src="https://i0.wp.com/blog.ugreen.com/wp-content/uploads/2020/10/iphone-12-banner-1.png?fit=1140%2C680&ssl=1"
+          className="size-full object-cover object-center"
+          src={data?.photo}
           alt=""
         />
       </figure>
 
-      <p className="mt-8">
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Id vitae odio
-        soluta dolores facilis repellat quisquam veritatis commodi ipsam vel
-        amet consequatur explicabo nam magni incidunt doloremque, eveniet
-        voluptatibus, impedit aliquam velit illum! Excepturi nihil numquam
-        consectetur doloremque molestias atque quo, recusandae provident
-        repellendus placeat rem velit laudantium ad unde blanditiis! Magni
-        obcaecati, porro id, debitis nihil voluptas sit, temporibus in at
-        dolores dolore! Tempora autem repellendus quas itaque sed beatae
-        officiis! Molestias, dignissimos. Eveniet sed asperiores, doloribus
-        minima reprehenderit, modi porro quam animi atque quos temporibus
-        praesentium. Inventore deserunt deleniti consequuntur at sed numquam
-        asperiores dolorem dicta officia explicabo? Lorem ipsum dolor sit amet
-        consectetur adipisicing elit. Alias culpa sunt tempore laudantium magnam
-        necessitatibus expedita nisi atque reiciendis sequi amet inventore
-        laborum aspernatur nihil impedit quos et debitis praesentium dolores
-        porro neque, quidem repellendus velit. A sit officia sint totam quasi
-        enim ad. Laudantium eius harum quisquam repellendus aspernatur
-        consequatur excepturi, inventore magnam praesentium, ducimus quasi
-        necessitatibus, voluptatum in ut expedita quod. Delectus, ducimus vitae
-        placeat esse in sapiente quibusdam, eligendi, laudantium iusto quisquam
-        officia fugiat obcaecati hic fuga nihil magni distinctio aut? Nemo
-        rerum, tempora officiis deserunt commodi nesciunt sunt blanditiis
-        numquam ullam aliquid, provident cupiditate sed! Id!
-      </p>
+      <div
+        className="mt-7"
+        dangerouslySetInnerHTML={{ __html: data?.content }}
+      ></div>
 
+      <div className="mb-5 mt-7 border-b border-gray-200 pb-5 text-center">
+        <h3 className="mb-2 text-paragraphColor">
+          {moment(data?.published_date, "YYYYMMDD").fromNow()}
+        </h3>
+        <Link to={""}>by {data.author?.fullname}</Link>
+      </div>
       <Comment />
     </>
   );
