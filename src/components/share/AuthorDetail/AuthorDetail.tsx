f31bbc72555ec7e0fda6f11f3e44a65ea4ffbdf3
AzarAhmadov <ehmedovh111@gmail.com>
2024-08-26 12:50:43 +0400

    Create AuthorDetail.tsx

---
+++ src/components/share/AuthorDetail/AuthorDetail.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Author from "../../ui/Author";
+
+const AuthorDetail: React.FC = () => {
+  return (
+    <>
+      <Author />
+    </>
+  );
+};
+
+export default AuthorDetail;
