e2968cd82ee6b5e750d7c1e3a564eb42f16fb265
AzarAhmadov <ehmedovh111@gmail.com>
2024-08-27 12:18:01 +0400

    Update NewsBlock.tsx

--- src/components/share/NewsBlock/NewsBlock.tsx
+++ src/components/share/NewsBlock/NewsBlock.tsx
@@ -1,22 +1,41 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import NewsBlockItem from "./NewsBlockItem/NewsBlockItem";
 import { typeNewsBlock } from "../../../types/types";
 import { useFetchAllNews } from "../../../hooks/useFetch";
+import LoadMore from "../../ui/LoadMore";
 
 const NewsBlock: React.FC<typeNewsBlock> = ({ title, icon }) => {
-  const [data] = useFetchAllNews();
+  const [page, setPage] = useState(1);
+  const newsLimit = 10;
+  const [data] = useFetchAllNews(page, newsLimit);
+  const [newsData, setNewsData] = useState([]);
+  const [hasMoreData, setHasMoreData] = useState(true);
+
+  const handlePage = () => {
+    setPage((p) => p + 1);
+  };
+
+  useEffect(() => {
+    if (data && data.length > 0) {
+      setNewsData((prevNewsData) => [...prevNewsData, ...data]);
+      if (data.length < newsLimit) {
+        setHasMoreData(false);
+      }
+    }
+  }, [data]);
 
   return (
     <>
-      <div className="mt-6 flex items-center gap-x-2">
+      <div className="flex items-center mt-6 gap-x-2">
         {icon}
         <h2 className="text-lg font-medium"> {title} </h2>
       </div>
       <div className="grid grid-cols-3 gap-3 py-5">
-        {data?.map((item: any, index: number) => (
+        {newsData?.map((item: any, index: number) => (
           <NewsBlockItem item={item} key={index} />
         ))}
       </div>
+      {hasMoreData && <LoadMore handlePage={handlePage} />}
     </>
   );
 };
