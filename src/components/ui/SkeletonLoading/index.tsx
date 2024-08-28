0a8bb4c53c3f2311d83ca968aa4dc174db6bda2b
AzarAhmadov <ehmedovh111@gmail.com>
2024-08-28 15:28:48 +0400

    Update index.tsx

--- src/components/ui/SkeletonLoading/index.tsx
+++ src/components/ui/SkeletonLoading/index.tsx
@@ -1,30 +1,5 @@
-import classNames from "classnames";
 import React from "react";
-import "./skeleton.css";
 import Skeleton from "react-loading-skeleton";
 import "react-loading-skeleton/dist/skeleton.css";
 
-const SkeletonLoading: React.FC<any> = ({
-  count,
-  type,
-  width,
-  height,
-  rounded = false,
-}) => {
-  const RenderSkeleton = () => {
-    if (type === "navbar")
-      return (
-        <Skeleton
-          className={classNames({ "mb-4": true, "!rounded-r-full": rounded }, [
-            `skeleton-${width}`,
-            `skeleton-h-${height} `,
-          ])}
-          count={count}
-        />
-      );
-  };
-
-  return <>{RenderSkeleton()}</>;
-};
-
-export default SkeletonLoading;
+export const NavbarSkeleton = () => {};
