e3dcb4ff82a7b03d87f99d33240c8390d38928cf
AzarAhmadov <ehmedovh111@gmail.com>
2024-08-28 15:28:55 +0400

    Update index.tsx

--- src/components/ui/Breadcrumb/index.tsx
+++ src/components/ui/Breadcrumb/index.tsx
@@ -1,52 +1,52 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
+import { TextFirstLineUpperCase } from "../../../helper/helper";
 
 const Breadcrumb: React.FC = () => {
+  const location = useLocation();
+  const { pathname, search } = location;
+  const category = search.split("=")[1];
+  const segments = pathname.split("/");
+  let url = "";
+
   return (
     <div className="mt-6 mb-2">
       <nav className="flex" aria-label="Breadcrumb">
-        <ol className="inline-flex items-center space-x-1 md:space-x-2 rtl:space-x-reverse">
-          <li className="inline-flex items-center">
-            <Link
-              to="/"
-              className="inline-flex items-center text-sm font-medium text-gray-700 hover:text-blue-600"
-            >
-              <svg
-                className="me-2.5 h-3 w-3"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-              >
-                <path d="m19.707 9.293-2-2-7-7a1 1 0 0 0-1.414 0l-7 7-2 2a1 1 0 0 0 1.414 1.414L2 10.414V18a2 2 0 0 0 2 2h3a1 1 0 0 0 1-1v-4a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v4a1 1 0 0 0 1 1h3a2 2 0 0 0 2-2v-7.586l.293.293a1 1 0 0 0 1.414-1.414Z" />
-              </svg>
-              Home
-            </Link>
-          </li>
-          <li aria-current="page">
-            <div className="flex items-center">
-              <svg
-                className="w-3 h-3 mx-1 text-gray-400 rtl:rotate-180"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 6 10"
-              >
-                <path
-                  stroke="currentColor"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  d="m1 9 4-4-4-4"
-                />
-              </svg>
-              <span className="text-sm font-medium text-gray-500 ms-1 md:ms-2 dark:text-gray-400">
-                Search
-              </span>
-            </div>
-          </li>
-          <li aria-current="page">
-            <div className="flex items-center">
+        <ul className="inline-flex items-center space-x-1 md:space-x-2 rtl:space-x-reverse">
+          {segments.map((segment, i) => {
+            url += `${segment}`;
+            return (
+              <li key={i} aria-current="page">
+                <div className="flex items-center">
+                  {url.length > 1 && (
+                    <svg
+                      className="w-3 h-3 mx-1 text-gray-400 rtl:rotate-180"
+                      aria-hidden="true"
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 6 10"
+                    >
+                      <path
+                        stroke="currentColor"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth={2}
+                        d="M1 9L5 5 1 1"
+                      />
+                    </svg>
+                  )}
+                  <Link
+                    to={`/${url}`}
+                    className="text-sm font-medium text-gray-500 transition-all hover:text-black ms-1 md:ms-2"
+                  >
+                    {segment === "" ? "Home" : TextFirstLineUpperCase(segment)}
+                  </Link>
+                </div>
+              </li>
+            );
+          })}
+          {category && (
+            <li className="flex items-center">
               <svg
                 className="w-3 h-3 mx-1 text-gray-400 rtl:rotate-180"
                 aria-hidden="true"
@@ -59,15 +59,15 @@ const Breadcrumb: React.FC = () => {
                   strokeLinecap="round"
                   strokeLinejoin="round"
                   strokeWidth={2}
-                  d="m1 9 4-4-4-4"
+                  d="M1 9L5 5 1 1"
                 />
               </svg>
               <span className="text-sm font-medium text-gray-500 ms-1 md:ms-2 dark:text-gray-400">
-                Flowbite
+                {TextFirstLineUpperCase(category)}
               </span>
-            </div>
-          </li>
-        </ol>
+            </li>
+          )}
+        </ul>
       </nav>
     </div>
   );
