6b7fe533f3b5e5c4e5fcbfa8f823994ba3ee4651
AzarAhmadov <ehmedovh111@gmail.com>
2024-09-01 23:29:06 +0400

    Update auth.service.ts

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,10 +1,12 @@
 import UserApi from "../api/user.api";
 import { API } from "../utils/api";
+import { serviceSetToken, serviceSetUser } from "./app.service";
 
 export const serviceAuthLogin = async (data: any) => {
   try {
     const res = await API.post(UserApi.login, data);
-    console.log(res)
+    serviceSetToken(res.token);
+    serviceSetUser(res.user);
   } catch (e) {
     return false;
   }
