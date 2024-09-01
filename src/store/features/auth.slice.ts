9fab3b36a1266729b67c3d40c067a4d648d5d9d5
AzarAhmadov <ehmedovh111@gmail.com>
2024-09-01 23:29:21 +0400

    Create auth.slice.ts

---
+++ src/store/features/auth.slice.ts
@@ -0,0 +1,41 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { useSelector } from "react-redux";
+import { RootState } from "../app.store";
+
+interface IinitialState {
+  user: {
+    photo: string;
+    name: string;
+    surname: string;
+  };
+  token: string;
+}
+
+const initialState: IinitialState = {
+  token: localStorage.getItem("token") || "",
+  user: localStorage.getItem("user")
+    ? JSON.parse(localStorage.getItem("user"))
+    : false,
+};
+
+const AuthSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {
+    setUser: (state, action: PayloadAction<string>) => {
+      state.user = action.payload;
+      localStorage.setItem("user", JSON.stringify(state.user));
+    },
+    setToken: (state, action: PayloadAction<string>) => {
+      state.token = action.payload;
+      localStorage.setItem("token", state.token);
+    },
+  },
+});
+
+export const { setUser, setToken } = AuthSlice.actions;
+
+export const useStoreAuth = () =>
+  useSelector((state: RootState) => state.AuthStore);
+
+export default AuthSlice.reducer;
