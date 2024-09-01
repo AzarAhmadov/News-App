b838a1c6fd3acdb932564c4993506436fd66817b
AzarAhmadov <ehmedovh111@gmail.com>
2024-09-01 23:28:20 +0400

    Update Modal.tsx

--- src/components/common/Modals/Modal/Modal.tsx
+++ src/components/common/Modals/Modal/Modal.tsx
@@ -1,7 +1,10 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { IoMdClose } from "react-icons/io";
 import Button from "../../../ui/Button";
 import { serviceAuthLogin } from "../../../../services/auth.service";
+import { useStoreApp } from "../../../../store/features/app.slice";
+import classNames from "classnames";
+import { serviceSetToggleStatus } from "../../../../services/app.service";
 
 const Modal: React.FC = () => {
   const [inputs, setInputs] = useState({
@@ -24,12 +27,31 @@ const Modal: React.FC = () => {
     serviceAuthLogin(inputs);
   };
 
+  const { toggle } = useStoreApp();
+
+  useEffect(() => {
+    if (toggle) {
+      document.body.classList.add("overflow-hidden");
+    } else {
+      document.body.classList.remove("overflow-hidden");
+    }
+  }, [toggle]);
+
   return (
-    <div className="fixed inset-0 z-10 grid p-1 place-items-center backdrop-blur-sm">
+    <div
+      className={classNames({
+        "visible fixed inset-0 z-10 grid h-auto scale-100 place-items-center p-1 opacity-100 backdrop-blur-sm transition-all duration-200":
+          toggle,
+        "invisible scale-75 opacity-0 h-0": !toggle,
+      })}
+    >
       <div className="w-full max-w-[450px] rounded-md border border-gray-200 bg-white">
         <div className="flex items-center justify-between p-5 border-b">
           <h4 className="text-xl">Login</h4>
-          <IoMdClose className="cursor-pointer text-[1.5rem]" />
+          <IoMdClose
+            onClick={serviceSetToggleStatus}
+            className="cursor-pointer text-[1.5rem]"
+          />
         </div>
         <div className="py-7 pe-5 ps-5">
           <form onSubmit={handleSubmit} className="flex flex-col gap-y-5">
@@ -59,7 +81,12 @@ const Modal: React.FC = () => {
                 id="password"
               />
             </div>
-            <Button size="sm" type="submit" variant="black" rounded={true}>
+            <Button
+              size="sm"
+              type="submit"
+              variant="black"
+              rounded={true}
+            >
               Login
             </Button>
           </form>
