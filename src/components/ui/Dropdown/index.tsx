eca39c81931c17a8c6d13dde94e2d427f3d9e1ac
AzarAhmadov <ehmedovh111@gmail.com>
2024-09-01 23:28:40 +0400

    Create dropdown ui

---
+++ src/components/ui/Dropdown/index.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { useClickAway } from "@uidotdev/usehooks";
+import { IoIosArrowDown, IoIosArrowUp } from "react-icons/io";
+
+const Dropdown: React.FC<any> = ({ DropdownHeader, children }) => {
+  const [isOpen, setIsOpen] = React.useState(false);
+  const ref = useClickAway(() => {
+    setIsOpen(false);
+  });
+
+  const handleOpenModal = () => {
+    setIsOpen((prev) => !prev);
+  };
+
+  return (
+    <>
+      <div className="relative flex" onClick={handleOpenModal}>
+        <div className="flex items-center px-4 border border-gray-300 rounded-md gap-x-3">
+          {DropdownHeader}
+          {isOpen ? <IoIosArrowUp /> : <IoIosArrowDown />}
+        </div>
+        {isOpen && <div ref={ref}>{children}</div>}
+      </div>
+    </>
+  );
+};
+
+export default Dropdown;
