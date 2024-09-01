6ef851dc815f5de330bb51e845aea7fdf6fa2e04
AzarAhmadov <ehmedovh111@gmail.com>
2024-09-01 23:28:58 +0400

    Update AuthorDetail.tsx

--- src/pages/AuthorDetail/AuthorDetail.tsx
+++ src/pages/AuthorDetail/AuthorDetail.tsx
@@ -2,11 +2,7 @@ import React from "react";
 import Author from "../../components/ui/Author";
 
 const AuthorDetail: React.FC = () => {
-  return (
-    <>
-      <Author />
-    </>
-  );
+  return <Author />;
 };
 
 export default AuthorDetail;
