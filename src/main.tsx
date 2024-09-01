627283a3feaab8861bcc477c375a1841d18236c3
AzarAhmadov <ehmedovh111@gmail.com>
2024-09-01 23:28:55 +0400

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,11 @@ import { createRoot } from "react-dom/client";
 import "./assets/css/index.css";
 import { RouterProvider } from "react-router-dom";
 import Routers from "./router/router.tsx";
+import { Provider } from "react-redux";
+import { store } from "./store/app.store.ts";
 
 createRoot(document.getElementById("root")!).render(
-  <RouterProvider router={Routers} />,
+  <Provider store={store}>
+    <RouterProvider router={Routers} />
+  </Provider>,
 );
