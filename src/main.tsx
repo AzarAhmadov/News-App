d15475735f3c1a121f542b113f1d374e956c0e19
AzarAhmadov <ehmedovh111@gmail.com>
2024-09-02 23:13:08 +0400

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -4,9 +4,14 @@ import { RouterProvider } from "react-router-dom";
 import Routers from "./router/router.tsx";
 import { Provider } from "react-redux";
 import { store } from "./store/app.store.ts";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 createRoot(document.getElementById("root")!).render(
-  <Provider store={store}>
-    <RouterProvider router={Routers} />
-  </Provider>,
+  <>
+    <Provider store={store}>
+      <RouterProvider router={Routers} />
+    </Provider>
+    <ToastContainer />
+  </>,
 );
