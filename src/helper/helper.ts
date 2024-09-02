2e92b718ddf9908c0c80e037373c2dae6856a32e
AzarAhmadov <ehmedovh111@gmail.com>
2024-09-02 23:13:13 +0400

    Update helper.ts

--- src/helper/helper.ts
+++ src/helper/helper.ts
@@ -1,3 +1,9 @@
+import { toast } from "react-toastify";
+
 export const TextFirstLineUpperCase = (text: string) => {
   return text[0].toUpperCase() + text.slice(1);
+};
+
+export const notification = (type: string, message: string) => {
+  toast[type](message);
 };
