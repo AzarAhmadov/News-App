bb992c29c639b84064d10899e0aab7fa9e6a5174
AzarAhmadov <ehmedovh111@gmail.com>
2024-09-02 23:13:02 +0400

    Update app.slice.ts

--- src/store/features/app.slice.ts
+++ src/store/features/app.slice.ts
@@ -4,12 +4,16 @@ import { RootState } from "../app.store";
 
 interface IinitialState {
   toggle: boolean;
+  darkMode: any;
   errors: any;
 }
 
 const initialState: IinitialState = {
   toggle: false,
   errors: {},
+  darkMode: localStorage.getItem("darkMode")
+    ? localStorage.getItem("darkMode") === "true"
+    : window.matchMedia("(prefers-color-scheme: dark)").matches,
 };
 
 export const AppSlice = createSlice({
@@ -22,10 +26,14 @@ export const AppSlice = createSlice({
     setToggle: (state) => {
       state.toggle = !state.toggle;
     },
+    setDarkMode: (state, action) => {
+      state.darkMode = action.payload;
+      localStorage.setItem("darkMode", state.darkMode);
+    },
   },
 });
 
-export const { setToggle, setErrors } = AppSlice.actions;
+export const { setToggle, setErrors, setDarkMode } = AppSlice.actions;
 
 export const useStoreApp = () =>
   useSelector((state: RootState) => state.AppStore);
