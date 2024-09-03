c27dd736d3ce82f5e8f02b0aa56e966c83959252
AzarAhmadov <ehmedovh111@gmail.com>
2024-09-03 12:19:55 +0400

    Update CenterBlock.tsx

--- src/components/share/CenterBlock/CenterBlock.tsx
+++ src/components/share/CenterBlock/CenterBlock.tsx
@@ -3,11 +3,10 @@ import Slide from "../../ui/Swiper";
 import { LuPencilLine } from "react-icons/lu";
 import { SwiperSlide } from "swiper/react";
 import NewsBlock from "../NewsBlock/NewsBlock";
-import Button from "../../ui/Button";
-import { MdArrowForwardIos } from "react-icons/md";
 import { BiWorld } from "react-icons/bi";
 import { FaRegNewspaper } from "react-icons/fa6";
 import { useFetchAuthors } from "../../../hooks/useFetch";
+import AuthorCard, { IauthorCard } from "../../ui/AuthorCard";
 
 const CenterBlock: React.FC = () => {
   const [data] = useFetchAuthors();
@@ -19,37 +18,9 @@ const CenterBlock: React.FC = () => {
         icon={<BiWorld className="text-xl" />}
       />
       <Slide title="Creators you should follow" icon={<LuPencilLine />}>
-        {data?.data?.map((item: any, index: number) => (
+        {data?.data?.map((item: IauthorCard["item"], index: number) => (
           <SwiperSlide key={index}>
-            <div className="flex flex-col items-center p-4 bg-white rounded-md dark:bg-gray-800 gap-y-3">
-              <figure className="size-[85px] overflow-hidden rounded-full border border-black/10 p-1">
-                <img
-                  className="object-cover rounded-full size-full"
-                  src={item.photo}
-                  alt={item.fullname}
-                />
-              </figure>
-              <div className="text-center">
-                <h3 className="text-md line-clamp-1">{item.fullname}</h3>
-                <span className="text-sm line-clamp-1 text-paragraphColor dark:text-white">
-                  {item.agency}
-                </span>
-              </div>
-              <Button
-                outline={true}
-                width="full"
-                size="md"
-                type="button"
-                variant="black"
-                rounded={true}
-                to={`author/${item.slug}`}
-              >
-                <span className="flex items-center justify-center gap-x-2">
-                  Detail
-                  <MdArrowForwardIos />
-                </span>
-              </Button>
-            </div>
+            <AuthorCard item={item} />
           </SwiperSlide>
         ))}
       </Slide>
