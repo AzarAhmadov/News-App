cbe62000200a0559553d7fae581833fe165a1603
AzarAhmadov <ehmedovh111@gmail.com>
2024-09-03 12:19:32 +0400

    Update helper.ts

--- src/helper/helper.ts
+++ src/helper/helper.ts
@@ -4,6 +4,9 @@ export const TextFirstLineUpperCase = (text: string) => {
   return text[0].toUpperCase() + text.slice(1);
 };
 
-export const notification = (type: string, message: string) => {
+export const notification = (
+  type: "success" | "error" | "info" | "warning",
+  message: string,
+) => {
   toast[type](message);
 };
