0ba40b64234d7a317fa14c879cc57ac3ac7431fd
AzarAhmadov <ehmedovh111@gmail.com>
2024-09-03 23:42:39 +0400

    Update Search.tsx

--- src/pages/Search/Search.tsx
+++ src/pages/Search/Search.tsx
@@ -1,15 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import { useSearchParams } from "react-router-dom";
 import { useFetchNewsByCategory } from "../../hooks/useFetch";
 import Spinner from "../../components/ui/Spinner";
 import ErrorMsg from "../../components/ui/ErrorMsg";
 import NewsBlockItem from "../../components/share/NewsBlock/NewsBlockItem/NewsBlockItem";
+import Pagination from "../../components/ui/Pagination";
 
 const Search: React.FC = () => {
   const [searchParams] = useSearchParams();
   const category = searchParams.get("category");
-
-  const [data, loading] = useFetchNewsByCategory(category);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [data, loading] = useFetchNewsByCategory(category, currentPage,9);
+  const totalPages = 5;
 
   if (loading) {
     return <Spinner />;
@@ -27,6 +29,14 @@ const Search: React.FC = () => {
         {data?.map((item: any, index: number) => (
           <NewsBlockItem item={item} key={index} />
         ))}
+      </div>
+
+      <div className="pt-4 pb-7">
+        <Pagination
+          currentPage={currentPage}
+          totalPages={totalPages}
+          onPageChange={setCurrentPage}
+        />
       </div>
     </>
   );
