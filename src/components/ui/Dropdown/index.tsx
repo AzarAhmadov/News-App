0675e8be3344de0f8779a806905532eef337d1cd
AzarAhmadov <ehmedovh111@gmail.com>
2024-09-03 12:19:42 +0400

    Update index.tsx

--- src/components/ui/Dropdown/index.tsx
+++ src/components/ui/Dropdown/index.tsx
@@ -4,7 +4,7 @@ import { IoIosArrowDown, IoIosArrowUp } from "react-icons/io";
 
 const Dropdown: React.FC<any> = ({ DropdownHeader, children }) => {
   const [isOpen, setIsOpen] = React.useState(false);
-  const ref = useClickAway(() => {
+  const ref: any = useClickAway(() => {
     setIsOpen(false);
   });
 
@@ -13,15 +13,13 @@ const Dropdown: React.FC<any> = ({ DropdownHeader, children }) => {
   };
 
   return (
-    <>
-      <div className="relative flex" onClick={handleOpenModal}>
-        <div className="flex items-center px-4 border border-gray-300 rounded-md dark:border-gray-600 gap-x-3">
-          {DropdownHeader}
-          {isOpen ? <IoIosArrowUp /> : <IoIosArrowDown />}
-        </div>
-        {isOpen && <div ref={ref}>{children}</div>}
+    <div className="relative flex" onClick={handleOpenModal}>
+      <div className="flex items-center gap-x-3 rounded-md border border-gray-300 px-4 dark:border-gray-600">
+        {DropdownHeader}
+        {isOpen ? <IoIosArrowUp /> : <IoIosArrowDown />}
       </div>
-    </>
+      {isOpen && <div ref={ref}>{children}</div>}
+    </div>
   );
 };
 
