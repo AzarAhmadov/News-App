a77b260e4d7124e69a6921ed34b975dee487c32f
AzarAhmadov <ehmedovh111@gmail.com>
2024-09-03 12:19:38 +0400

    Update index.tsx

--- src/components/ui/ErrorMsg/index.tsx
+++ src/components/ui/ErrorMsg/index.tsx
@@ -14,7 +14,7 @@ const ErrorMsg: React.FC<TypeErrorMsg> = ({ text }) => {
       <p className="mt-5 text-xl">{text}</p>
       <Link
         to={"/"}
-        className="flex items-center px-4 py-2 mt-5 text-white bg-black rounded-md gap-x-3"
+        className="mt-5 flex items-center gap-x-3 rounded-md bg-black px-4 py-2 text-white"
       >
         <FaArrowLeft />
         Go back
