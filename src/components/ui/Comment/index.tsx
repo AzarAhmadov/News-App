49dc7651b251fcd454760a28775f3130a2a8ebed
AzarAhmadov <ehmedovh111@gmail.com>
2024-09-04 11:25:02 +0400

    Update index.tsx

--- src/components/ui/Comment/index.tsx
+++ src/components/ui/Comment/index.tsx
@@ -1,54 +1,90 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { memo, ReactElement } from "react";
 import Button from "../Button";
 import { IoIosArrowDown } from "react-icons/io";
 import { AiOutlineDelete } from "react-icons/ai";
+import moment from "moment";
 
-const Comment: React.FC = () => {
-  return (
-    <>
-      <div className="mx-auto pb-7 w-full max-w-[55rem]">
-        <div className="text-center ">
-          <h3 className="mb-3 text-lg font-medium">Add your comment</h3>
-          <div className="flex flex-col items-center gap-y-3">
-            <textarea
-              className="h-[10rem] w-full resize-none rounded-lg p-3 outline-none"
-              placeholder="Enter your comment here.."
-            />
-            <Button type="submit" variant="primaryDark" rounded={true} size="sm">
-              Post Comment
-            </Button>
-          </div>
-        </div>
-        <div className="mt-5">
-          <h3 className="flex items-center gap-x-3">
-            View All Comments (04){" "}
-            <div className="grid size-[1.7rem] place-items-center rounded-full bg-primaryDarker text-white">
-              <IoIosArrowDown />
-            </div>
-          </h3>
+interface CommentItem {
+  body: ReactElement;
+  created_at: Date;
+  user: {
+    photo: string;
+    name: string;
+    surname: string;
+  };
+}
 
-          <ul className="mt-3 space-y-5">
-            <li>
-              <div className="flex items-center justify-between">
-                <h4 className="text-lg text-primary">Rakshan</h4>
-                <div className="p-2 text-xl text-white bg-red-600 rounded-md cursor-pointer">
-                  <AiOutlineDelete />
-                </div>
+interface CommentProps {
+  items: CommentItem[];
+}
+
+const Comment: React.FC<CommentProps> = ({ items = [] }) => {
+  console.log(items);
+  const renderCommentList = () => {
+    if (items && items.length > 0) {
+      if (items.length === 0) {
+        return <p>No comments to display</p>;
+      }
+
+      return items.map((item, index) => (
+        <li key={index}>
+          <div className="flex items-center justify-between">
+            <div>
+              <div className="flex items-center gap-x-3">
+                <figure className="size-[3rem] overflow-hidden">
+                  <img
+                    className="object-cover rounded-full size-full"
+                    src={item.user.photo}
+                    alt={item.user.name}
+                  />
+                </figure>
+                <h4 className="text-lg text-primary">
+                  {item.user.name}
+                  <span className="ms-1"> {item.user.surname}</span>
+                </h4>
               </div>
-              <p className="py-2 text-paragraphColor">
-                I’ve been a great fan of Samsung products since 2010. Can’t wait
-                for this one. 🔥🔥🔥
-              </p>
-              <span className="text-sm text-paragraphColor">
-                Posted on Jul 5, 2021 | 6:22 AM
-              </span>
-            </li>
-          </ul>
+            </div>
+            <div className="p-2 text-xl text-white bg-red-600 rounded-md cursor-pointer">
+              <AiOutlineDelete />
+            </div>
+          </div>
+          <p className="py-2 text-paragraphColor">{item.body}</p>
+          <span className="text-sm text-paragraphColor">
+            {moment(item.created_at).format("DD-MM-YYYY HH:mm")}
+          </span>
+        </li>
+      ));
+    } else {
+      return <p>No comments to display</p>;
+    }
+  };
+
+  return (
+    <div className="mx-auto w-full max-w-[55rem] pb-7">
+      <div className="text-center">
+        <h3 className="mb-3 text-lg font-medium">Add your comment</h3>
+        <div className="flex flex-col items-center gap-y-3">
+          <textarea
+            className="h-[10rem] w-full resize-none rounded-lg p-3 outline-none dark:bg-gray-700 dark:text-white dark:placeholder:text-white"
+            placeholder="Enter your comment here.."
+          />
+          <Button type="submit" variant="primaryDark" rounded={true} size="sm">
+            Post Comment
+          </Button>
         </div>
       </div>
-    </>
+      <div className="mt-5">
+        <h3 className="flex items-center gap-x-3">
+          View All Comments ({items.length})
+          <div className="grid size-[1.7rem] place-items-center rounded-full bg-primaryDarker text-white">
+            <IoIosArrowDown />
+          </div>
+        </h3>
+
+        <ul className="mt-3 space-y-10">{renderCommentList()}</ul>
+      </div>
+    </div>
   );
 };
 
-export default Comment;
+export default memo(Comment);
