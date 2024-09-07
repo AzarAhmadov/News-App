d78a56ad9089b2e8f9ee3571a1f379358392c14f
AzarAhmadov <ehmedovh111@gmail.com>
2024-09-07 19:42:24 +0400

    Update index.tsx

--- src/components/common/Language/index.tsx
+++ src/components/common/Language/index.tsx
@@ -7,16 +7,16 @@ const Language: React.FC = () => {
       DropdownHeader={
         <div className="flex items-center py-3 cursor-pointer gap-x-2 whitespace-nowrap">
           <img
-            className="h-[25px] w-[200px] rounded-sm object-cover"
+            className="h-[25px] min-w-[45px] rounded-sm object-cover"
             src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/Flag_of_Azerbaijan.svg/800px-Flag_of_Azerbaijan.svg.png"
             alt="Azerbaijan"
           />
         </div>
       }
     >
-      <div className="absolute -bottom-[3.6rem] left-[0.2rem] flex w-max cursor-pointer justify-center rounded-md border px-2 py-2 text-center text-white dark:border-gray-600">
+      <div className="absolute -bottom-[3rem] left-0 flex w-max cursor-pointer justify-center rounded-md border px-2 py-2 text-center text-white dark:border-gray-600">
         <img
-          className="h-[25px] w-[50px] rounded-sm object-cover"
+          className="h-[25px] w-[45px] rounded-sm object-cover"
           src="https://cdn.britannica.com/79/4479-050-6EF87027/flag-Stars-and-Stripes-May-1-1795.jpg"
           alt="America"
         />
