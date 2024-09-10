3c5fa1a11c55e67c6e75a06da9785aa8ee5dede4
AzarAhmadov <ehmedovh111@gmail.com>
2024-09-11 00:57:51 +0400

    Add translate helper.js to change language

--- src/helper/helper.ts
+++ src/helper/helper.ts
@@ -1,4 +1,7 @@
 import { toast } from "react-toastify";
+import langAz from "../locales/az.json";
+import langEn from "../locales/en.json";
+import { store } from "../store/app.store";
 
 export const TextFirstLineUpperCase = (text: string) => {
   return text[0].toUpperCase() + text.slice(1);
@@ -9,4 +12,29 @@ export const notification = (
   message: string,
 ) => {
   toast[type](message);
+};
+
+export const translate = (name: string) => {
+  const key = name.split(".");
+
+  const { language } = store.getState().AppStore;
+
+  const current_langauge = language;
+
+  const langauges = {
+    az: langAz,
+    en: langEn,
+  };
+
+  let result = langauges[current_langauge];
+
+  for (let k of key) {
+    if (result[k] !== undefined) {
+      result = result[k];
+    } else {
+      result = name;
+    }
+  }
+
+  return result;
 };
