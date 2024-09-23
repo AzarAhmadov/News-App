11c1f04ea7da4bb446b63b6c9e48e2eab1bbd1fd
AzarAhmadov <ehmedovh111@gmail.com>
2024-09-23 13:18:17 +0400

    Update Login.tsx

--- src/components/common/Modals/Login/Login.tsx
+++ src/components/common/Modals/Login/Login.tsx
@@ -1,33 +1,26 @@
-import React, { useState } from "react";
+import React, { FormEvent } from "react";
 import { IoMdClose } from "react-icons/io";
 import Button from "../../../ui/Button";
-import { serviceAuthLogin } from "../../../../services/auth.service";
 import classNames from "classnames";
 import { serviceSetToggleStatus } from "../../../../services/app.service";
 import { useStoreApp } from "../../../../store/features/app.slice";
+import useForm from "../../../../hooks/useForm";
+import { serviceAuthLogin } from "../../../../services/auth.service";
+import FormGroup from "../../../ui/Form/FormGroup";
+import FormInput from "../../../ui/Form/FormInput";
 
 const Login: React.FC = () => {
-  const [inputs, setInputs] = useState({
-    email: "",
-    password: "",
+  const { values, setField, handleSubmit } = useForm({
+    initialState: {
+      email: "",
+      password: "",
+    },
+    onSubmit: async (values: any) => {
+      await serviceAuthLogin(values);
+    },
   });
 
-  const handleChnage = (e: any) => {
-    const { name, value } = e.target;
-
-    setInputs((prev) => ({
-      ...prev,
-      [name]: value,
-    }));
-  };
-
-  const handleSubmit = (e: any) => {
-    e.preventDefault();
-
-    serviceAuthLogin(inputs);
-  };
-
-  const { toggle, errors } = useStoreApp();
+  const { toggle } = useStoreApp();
 
   if (toggle) {
     document.body.classList.add("overflow-hidden");
@@ -56,36 +49,21 @@ const Login: React.FC = () => {
         <div className="py-7 pe-5 ps-5">
           <form onSubmit={handleSubmit} className="flex flex-col gap-y-5">
             <div className="flex flex-col mb-2">
-              <label htmlFor="email">
-                Email <span className="text-red-500">*</span>
-              </label>
-              <input
-                onChange={handleChnage}
-                value={inputs.email}
-                name="email"
-                className={`mt-2 rounded-md border border-gray-400/40 p-2 outline-none dark:bg-gray-800 ${errors?.email ? "border-red-600" : "border-gray-400/40"}`}
-                type="email"
-                id="email"
-              />
-              {errors.email && (
-                <span className="mt-1 text-red-500">{errors?.email}</span>
-              )}
+              <FormGroup label="Email" required={true} error="email">
+                <FormInput
+                  value={values.email}
+                  onChange={(e: FormEvent) => setField("email", e)}
+                />
+              </FormGroup>
             </div>
             <div className="flex flex-col">
-              <label htmlFor="password">
-                Password <span className="text-red-500">*</span>
-              </label>
-              <input
-                onChange={handleChnage}
-                value={inputs.password}
-                name="password"
-                className={`mt-2 rounded-md border border-gray-400/40 p-2 outline-none dark:bg-gray-800 ${errors?.password ? "border-red-600" : "border-gray-400/40"}`}
-                type="password"
-                id="password"
-              />
-              {errors.password && (
-                <span className="mt-1 text-red-500">{errors?.password}</span>
-              )}
+              <FormGroup required={true} label="Password" error="password">
+                <FormInput
+                  password={true}
+                  value={values.password}
+                  onChange={(e: FormEvent) => setField("password", e)}
+                />
+              </FormGroup>
             </div>
             <Button size="sm" type="submit" variant="black" rounded={true}>
               Login
