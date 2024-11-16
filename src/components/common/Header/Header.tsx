e208ea0a3bf890a10d885cac7123d96a52716de2
AzarAhmadov <ehmedovh111@gmail.com>
2024-11-16 23:13:42 +0400

    Update Header.tsx

--- src/components/common/Header/Header.tsx
+++ src/components/common/Header/Header.tsx
@@ -1,17 +1,20 @@
 import React from "react";
-import { IoIosSearch, IoIosArrowForward } from "react-icons/io";
+import { IoIosArrowForward } from "react-icons/io";
 import { Link } from "react-router-dom";
 import { MdLogin } from "react-icons/md";
 import { serviceSetToggleStatus } from "../../../services/app.service";
 import { useStoreAuth } from "../../../store/features/auth.slice";
 import { CiLogout } from "react-icons/ci";
+import { FaBars } from "react-icons/fa6";
 import Theme from "../Theme";
 import Language from "../Language";
 import Dropdown from "../../ui/Dropdown";
 import { translate } from "../../../helper/helper";
+import useWidth from "../../../hooks/useWidth";
 
 const Header: React.FC = () => {
   const { token, user } = useStoreAuth();
+  const width = useWidth();
 
   const Lagout = () => {
     localStorage.removeItem("token");
@@ -19,61 +22,67 @@ const Header: React.FC = () => {
   };
 
   return (
-    <header className="flex items-center gap-x-5">
-      <form className="flex items-center justify-between w-full px-3 py-3 border border-gray-300 rounded-md gap-x-3 bg-inputBg dark:border-gray-600 dark:bg-gray-800">
+    <header className="flex items-center justify-between gap-x-5">
+      {/* <form className="flex items-center justify-between w-full px-3 py-3 border border-gray-300 rounded-md gap-x-3 bg-inputBg dark:border-gray-600 dark:bg-gray-800">
         <input
           className="w-full bg-transparent outline-none"
           type="text"
           placeholder={translate("form.input")}
         />
         <IoIosSearch className="text-[25px]" />
-      </form>
-      <Link
-        className="flex items-center px-6 py-3 transition-all border border-gray-300 rounded-md gap-x-4 whitespace-nowrap hover:bg-gray-200 dark:border-gray-600 dark:hover:bg-gray-700"
-        to={"/"}
-      >
-        <p className="flex gap-x-2">{translate("header.text")}</p>
-        <IoIosArrowForward className="text-[20px]" />
-      </Link>
-      {token ? (
-        <Dropdown
-          DropdownHeader={
-            <div className="flex items-center cursor-pointer gap-x-3 whitespace-nowrap">
-              <figure className="size-[3rem] flex-shrink-0 overflow-hidden rounded-full p-1">
-                <img
-                  className="object-cover rounded-full size-full"
-                  src={user?.photo}
-                  alt={user?.name}
-                />
-              </figure>
-              <h3>
-                {user?.name}
-                <span className="text-sm ms-1">{user?.surname}</span>
-              </h3>
-            </div>
-          }
+      </form> */}
+      {width > 992 ? (
+        <Link
+          className="flex items-center px-6 py-3 transition-all border border-gray-300 rounded-md gap-x-4 whitespace-nowrap hover:bg-gray-200 dark:border-gray-600 dark:hover:bg-gray-700"
+          to={"/"}
         >
-          <div className="absolute -bottom-[3.3rem] left-0 w-full rounded-md bg-primaryDark p-3 text-center text-white">
-            <button
-              onClick={Lagout}
-              className="mx-auto flex items-center justify-center gap-x-3 font-[500] text-white"
-            >
-              <CiLogout className="text-xl" /> {translate("auth.logout")}
-            </button>
-          </div>
-        </Dropdown>
+          <p className="flex gap-x-2">{translate("header.text")}</p>
+          <IoIosArrowForward className="text-[20px]" />
+        </Link>
       ) : (
-        <button
-          className="flex items-center px-10 py-3 text-white rounded-md gap-x-2 whitespace-nowrap bg-primaryDarker"
-          onClick={serviceSetToggleStatus}
-        >
-          {translate("auth.login")}
-          <MdLogin className="text-xl" />
-        </button>
+        <FaBars className="size-[25px]" />
       )}
+      <div className="flex items-center gap-x-3 md:gap-x-6">
+        {token ? (
+          <Dropdown
+            DropdownHeader={
+              <div className="flex items-center cursor-pointer gap-x-3 whitespace-nowrap">
+                <figure className="size-[3rem] flex-shrink-0 overflow-hidden rounded-full p-1">
+                  <img
+                    className="object-cover rounded-full size-full"
+                    src={user?.photo}
+                    alt={user?.name}
+                  />
+                </figure>
+                <h3>
+                  {user?.name}
+                  <span className="text-sm ms-1">{user?.surname}</span>
+                </h3>
+              </div>
+            }
+          >
+            <div className="absolute -bottom-[3.3rem] left-0 w-full rounded-md bg-primaryDark p-3 text-center text-white">
+              <button
+                onClick={Lagout}
+                className="mx-auto flex items-center justify-center gap-x-3 font-[500] text-white"
+              >
+                <CiLogout className="text-xl" /> {translate("auth.logout")}
+              </button>
+            </div>
+          </Dropdown>
+        ) : (
+          <button
+            className="flex items-center px-6 py-2 text-white rounded-md md:py-3 md:px-10 gap-x-2 whitespace-nowrap bg-primaryDarker"
+            onClick={serviceSetToggleStatus}
+          >
+            {translate("auth.login")}
+            <MdLogin className="text-xl" />
+          </button>
+        )}
 
-      <Language />
-      <Theme />
+        <Language />
+        <Theme />
+      </div>
     </header>
   );
 };
