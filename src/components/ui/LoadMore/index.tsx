e00eba8d1ed8a91a4c4bf3fcf88aa494b3436eac
AzarAhmadov <ehmedovh111@gmail.com>
2024-11-16 23:13:31 +0400

    Update index.tsx

--- src/components/ui/LoadMore/index.tsx
+++ src/components/ui/LoadMore/index.tsx
@@ -1,19 +1,23 @@
 import React, { memo } from "react";
 import Button from "../Button";
 import { TypeLoadMore } from "../../../types/types";
+import { translate } from "../../../helper/helper";
+import useWidth from "../../../hooks/useWidth";
 
 const LoadMore: React.FC<TypeLoadMore> = ({ handlePage }) => {
+  const width = useWidth();
+
   return (
-    <div className="flex justify-center pb-5 pt-3">
+    <div className="flex justify-center pt-3 pb-5">
       <Button
         onClick={() => handlePage()}
-        size="lg"
+        size={width > 578 ? "lg" : "md"}
         type="button"
         variant="black"
         outline={true}
         rounded={true}
       >
-        Load More
+        {translate("loadMore.text")}
       </Button>
     </div>
   );
