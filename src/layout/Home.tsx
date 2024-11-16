924dd09df50ccefa32e2743bbd89eeba410d5c0c
AzarAhmadov <ehmedovh111@gmail.com>
2024-11-16 23:13:16 +0400

    Update Home.tsx

--- src/layout/Home.tsx
+++ src/layout/Home.tsx
@@ -3,11 +3,20 @@ import Aside from "../components/common/Aside/Aside";
 import { Outlet } from "react-router-dom";
 import Header from "../components/common/Header/Header";
 import Footer from "../components/common/Footer/Footer";
+import useWidth from "../hooks/useWidth";
 
 const Home: React.FC = () => {
+  const width = useWidth();
+
   return (
-    <div className="grid grid-cols-[260px_calc(100%_-_325px)] gap-[35px] pt-3">
-      <Aside />
+    <div
+      className={`grid ${
+        width > 992
+          ? "grid-cols-[260px_calc(100%_-_325px)]"
+          : "grid-cols-[100%]"
+      } gap-[35px] pt-3 px-5`}
+    >
+      {width > 992 && <Aside />}
       <div className="container flex flex-col justify-between h-screen mx-auto">
         <div>
           <Header />
